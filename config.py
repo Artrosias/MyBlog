de94532016e6900ecd35ef5580f98a32e70b69ed
Alessio <ale99_sami03@hotmail.com>
2022-11-16 17:35:36 +0100

    Db da sistemare

--- config.py
+++ config.py
@@ -1,8 +1,11 @@
 import os
+from dotenv import load_dotenv
 
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+load_dotenv(os.path.join(basedir, 'env'))
+load_dotenv(os.path.join(basedir, 'flaskenv'))
 class Config:
     SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, 'blog.db')
     SECRET_KEY = os.environ.get('SECRET_KEY')
